6fcd2dc26c40df4d13ef3a17e4b425e56bb0b126
Charles Donizete Alves dos Santos <82051427+CharlesDonizete@users.noreply.github.com>
2024-01-08 16:13:35 -0300

    63: Novo Control Flow do Angular

--- src/app/courses/containers/courses/courses.component.ts
+++ src/app/courses/containers/courses/courses.component.ts
@@ -13,7 +13,7 @@ import { CoursePage } from '../../model/course-page';
 import { MatPaginator, PageEvent, MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { CoursesListComponent } from '../../components/courses-list/courses-list.component';
-import { NgIf, AsyncPipe } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 
@@ -23,14 +23,13 @@ import { MatCardModule } from '@angular/material/card';
     styleUrls: ['./courses.component.scss'],
     standalone: true,
     imports: [
-        MatCardModule,
-        MatToolbarModule,
-        NgIf,
-        CoursesListComponent,
-        MatPaginatorModule,
-        MatProgressSpinnerModule,
-        AsyncPipe,
-    ],
+    MatCardModule,
+    MatToolbarModule,
+    CoursesListComponent,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    AsyncPipe
+],
 })
 export class CoursesComponent implements OnInit {
   courses$: Observable<CoursePage> | null = null;
